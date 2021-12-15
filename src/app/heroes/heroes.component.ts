c5d9b7b4b7d74f820e16332ea8de246069c14bf6
saivarma <sairevanth7373@gmail.com>
2021-12-15 18:03:53 +0530

    third-push

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +10,13 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
-  heroes = HEROES;
+  heroes: Hero[]=[];
 
   selectedHero? : Hero;
 
@@ -22,10 +24,17 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero : Hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   
   /*onSelect(hero : Hero){
     this.selectedHero = hero;
+
+    *Void
   }*/
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
 
 }
